850e66db24c6bc6768117a5c70a426be841a7a4b
badhon017 <badhonbrinta25@gmail.com>
2024-01-31 00:38:32 +0600

    add configuration file for spring security

--- src/main/java/com/JwtAuthentication/jwtAuthService/controllers/UserController.java
+++ src/main/java/com/JwtAuthentication/jwtAuthService/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -20,4 +21,9 @@ public class UserController {
     public List<User> getUser() {
         return userService.getUsers();
     }
+    @GetMapping("/current-user")
+    public String getCurrentUser(Principal principal) {
+        return principal.getName();
+    }
 }
+
